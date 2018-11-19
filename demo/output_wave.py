c14fb80ebb323697f084222345428077dcdcf7ef
Bernd Porr <mail@berndporr.me.uk>
2018-11-19 12:50:17 +0000

    Removed debug out

--- demo/output_wave.py
+++ demo/output_wave.py
@@ -8,7 +8,7 @@ if not result == 0:
 
 # as fast as possible just for testing
 while True:
-    print(dux.digital_out(0,0))
+    dux.digital_out(0,0)
     dux.analogue_out(0,0)
     dux.digital_out(0,1)
     dux.analogue_out(0,256)
