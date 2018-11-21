8d2994fc49e0bb321c696d27c366c543b302236c
Bernd Porr <mail@berndporr.me.uk>
2018-11-21 23:46:17 +0000

    new release

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ pyusbdux_module = Extension('_pyusbdux',
 
 						   
 setup (name = 'pyusbdux',
-       version = '1.2.2',
+       version = '1.2.3',
        author      = "Bernd Porr",
        author_email = "mail@berndporr.me.uk",
        url = "https://github.com/berndporr/pyusbdux",
