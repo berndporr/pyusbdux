87923ce59eaf5d407f29d3485c4cb94839d80598
Bernd Porr <mail@berndporr.me.uk>
2018-11-29 23:35:58 +0000

    Up one version number because of the spelling mistake

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ pyusbdux_module = Extension('_pyusbdux',
 
 						   
 setup (name = 'pyusbdux',
-       version = '1.4.0',
+       version = '1.4.1',
        author      = "Bernd Porr",
        author_email = "mail@berndporr.me.uk",
        url = "https://github.com/berndporr/pyusbdux",
