5dd850362f980a03a5e698612e12b3be0a3a017e
Bernd Porr <mail@berndporr.me.uk>
2022-11-15 13:41:51 +0000

    Fixes wrong comments.

--- examples/printdata_from_usbdux.py
+++ examples/printdata_from_usbdux.py
@@ -12,11 +12,12 @@ cb = DataCallback()
 # open default USBDUX
 dux.open()
 
-# starts to acquire 8 analogue channels at a rate of 250Hz
-# in the background. They will be stored in the ringbuffer
-# till the they are read via getSampleFromBuffer().
+# sampling from 8 channels
+# at a sampling rate of 250Hz.
 dux.start(cb,8,250)
 
+# Just idles till the user presses return.
 input()
 
+# Stops the data acquisition.
 dux.stop()
